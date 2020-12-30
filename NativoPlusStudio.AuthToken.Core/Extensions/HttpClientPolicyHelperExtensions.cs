a787e62ed31ff058df12c015590dc6960f98452d
Angel Ramos <aramos@techcu.com>
2020-12-30 09:49:35 -0800

    fixes

--- NativoPlusStudio.AuthToken.Core/Extensions/HttpClientPolicyHelperExtensions.cs
+++ NativoPlusStudio.AuthToken.Core/Extensions/HttpClientPolicyHelperExtensions.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using NativoPlusStudio.AuthToken.Core.Enums;
+﻿using NativoPlusStudio.AuthToken.Core.Enums;
 using NativoPlusStudio.AuthToken.Core.Interfaces;
 using Polly;
 using Polly.Retry;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,57 +13,73 @@ namespace NativoPlusStudio.AuthToken.Core.Extensions
 {
     public static class HttpClientPolicyHelperExtensions
     {
-        public static IHttpClientBuilder CreateTokenRefreshPolicyOnUnauthorized(this IHttpClientBuilder clientBuilder,
-            IAuthTokenGenerator generator,
+        public static AsyncRetryPolicy<HttpResponseMessage> CreateTokenRefreshPolicyOnUnauthorized(this IAuthTokenGenerator generator,
             HttpRequestMessage message,
             string protectedResource,
             Func<IAuthTokenGenerator, HttpRequestMessage, string, Task> asyncAction,
             int retryCount)
         {
             if (generator == null) throw new ArgumentNullException("IAuthTokenGenerator generator", "Must be initialized");
-            AsyncRetryPolicy<HttpResponseMessage> policy = generator.RetryOnAuthorization(message, protectedResource, asyncAction, retryCount);
 
-            return clientBuilder.AddPolicyHandler(policy);
+            return generator
+                .RetryPolicy(
+                    message, 
+                    protectedResource, 
+                    asyncAction, 
+                    retryCount,
+                    HttpStatusCode.Unauthorized
+                );
         }
 
-        public static IHttpClientBuilder CreateTokenRefreshPolicyOnUnauthorized(this IHttpClientBuilder clientBuilder,
-            IAuthTokenGenerator generator,
+        public static AsyncRetryPolicy<HttpResponseMessage> CreateTokenRefreshPolicyOnUnauthorized(this IAuthTokenGenerator generator,
             HttpRequestMessage message,
             string protectedResource,
             Func<IAuthTokenGenerator, HttpRequestMessage, string, Task> asyncAction,
-            BackOffAlgorithmTypeEnums backOffType,
+            BackoffAlgorithmTypeEnums backOffType,
             int initialDelayInSeconds,
             int retryCount)
         {
             if (generator == null) throw new ArgumentNullException("IAuthTokenGenerator generator", "Must be initialized");
-            AsyncRetryPolicy<HttpResponseMessage> policy = generator.WaitAndRetryWithBackOffOnAuthorization(message, protectedResource, asyncAction, backOffType.GenerateBackOffTimeSpans(initialDelayInSeconds, retryCount));
 
-            return clientBuilder.AddPolicyHandler(policy);
+            return generator
+                .WaitAndRetryPolicyWithBackoff(
+                    message, 
+                    protectedResource, 
+                    asyncAction, 
+                    backOffType.GenerateBackoffDelay(initialDelayInSeconds, retryCount),
+                    HttpStatusCode.Unauthorized
+                );
         }
 
-        public static AsyncRetryPolicy<HttpResponseMessage> RetryOnAuthorization(this IAuthTokenGenerator generator,
+        public static AsyncRetryPolicy<HttpResponseMessage> RetryPolicy(this IAuthTokenGenerator generator,
             HttpRequestMessage message,
             string protectedResource,
             Func<IAuthTokenGenerator, HttpRequestMessage, string, Task> asyncAction,
-            int retryCount)
+            int retryCount,
+            params HttpStatusCode[] httpStatusCodes
+            )
         {
+            httpStatusCodes = httpStatusCodes.Any() ? httpStatusCodes : new HttpStatusCode[] { HttpStatusCode.Unauthorized };
             return Policy
-                .HandleResult<HttpResponseMessage>(message => message.StatusCode == HttpStatusCode.Unauthorized)
+                .HandleResult<HttpResponseMessage>(message => httpStatusCodes.Contains(message.StatusCode))
                 .RetryAsync(retryCount, async (result, retryCount, context) =>
                 {
                     await asyncAction.Invoke(generator, message, protectedResource);
                 });
         }
 
-        public static AsyncRetryPolicy<HttpResponseMessage> WaitAndRetryWithBackOffOnAuthorization(this IAuthTokenGenerator generator,
+        public static AsyncRetryPolicy<HttpResponseMessage> WaitAndRetryPolicyWithBackoff(this IAuthTokenGenerator generator,
             HttpRequestMessage message,
             string protectedResource,
             Func<IAuthTokenGenerator, HttpRequestMessage, string, Task> asyncAction,
-            IEnumerable<TimeSpan> delay
+            IEnumerable<TimeSpan> delay,
+            params HttpStatusCode[] httpStatusCodes
             )
         {
+            httpStatusCodes = httpStatusCodes.Any() ? httpStatusCodes : new HttpStatusCode[] { HttpStatusCode.Unauthorized };
+
             return Policy
-                .HandleResult<HttpResponseMessage>(message => message.StatusCode == HttpStatusCode.Unauthorized)
+                .HandleResult<HttpResponseMessage>(message => httpStatusCodes.Contains(message.StatusCode))
                 .WaitAndRetryAsync(delay, async (result, retryCount, context) =>
                 {
                     await asyncAction.Invoke(generator, message, protectedResource);
