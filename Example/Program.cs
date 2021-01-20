f58b7aad970eea1714209870f1d8f01799592a33
Angel Ramos <aramos@techcu.com>
2021-01-19 17:55:28 -0800

    example app and documentation

---
+++ Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using NativoPlusStudio.AuthToken.Core.Extensions;
+using NativoPlusStudio.AuthToken.Core.Interfaces;
+using Newtonsoft.Json;
+
+namespace Example
+{
+    class Program
+    {
+        public static IServiceProvider serviceProvider;
+        public static IAuthTokenGenerator authTokenGenerator;
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            var implementationName = "NameOfImplementation";
+            services.AddTokenProviderHelper(
+                protectedResourceName: implementationName,
+                () =>
+                {
+                    //necessary code to register you own implementation
+                    
+                    services.AddScoped<IAuthTokenProvider, ExampleTokenProvider>();
+                    //services.AddHttpClient<IAuthTokenProvider, ExampleTokenProvider>();
+                }
+            );
+
+            serviceProvider = services.BuildServiceProvider();
+
+            authTokenGenerator = serviceProvider.GetRequiredService<IAuthTokenGenerator>();
+
+            var token = authTokenGenerator.GetTokenAsync(protectedResource: implementationName).GetAwaiter().GetResult();
+            
+            Console.WriteLine(JsonConvert.SerializeObject(token));
+        }
+    }
+}
